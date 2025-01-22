97df9b21cc7ad1836f09a8a0343b0a214a05816a
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-22 23:30:39 +0100

    fix passing 0 or 1 to fibs() not returning correct value

--- fib.js
+++ fib.js
@@ -1,9 +1,8 @@
 function fibs(index) {
     let array = [0, 1];
 
-    if (index === 0 || index === 1) {
-        return array[index];
-    }
+    if (index === 0) return [0];
+    else if (index === 1) return array;
 
     for(let i = 2; i < index; i++) {
         array.push(array[i - 1] + array[i - 2]);
