89060372ec29deaf94078ec6fb8f419b4f19e0bd
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-22 23:37:01 +0100

    implement method for recursively generating fibonnaci sequence in an array

--- fib.js
+++ fib.js
@@ -10,3 +10,14 @@ function fibs(index) {
 
   return array;
 }
+
+function fibsRecursive(index) {
+  if (index === 0) return [0];
+  else if (index === 1) return [0, 1];
+
+  const previous = fibsRecursive(index - 1);
+
+  return previous.concat(
+    previous[previous.length - 1] + previous[previous.length - 2]
+  );
+}
