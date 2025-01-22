86c790454b616741e175bbbe84cc6bc2cf944105
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-22 23:31:00 +0100

    format fibs() with prettier

--- fib.js
+++ fib.js
@@ -1,12 +1,12 @@
 function fibs(index) {
-    let array = [0, 1];
+  let array = [0, 1];
 
-    if (index === 0) return [0];
-    else if (index === 1) return array;
+  if (index === 0) return [0];
+  else if (index === 1) return array;
 
-    for(let i = 2; i < index; i++) {
-        array.push(array[i - 1] + array[i - 2]);
-    }
+  for (let i = 2; i < index; i++) {
+    array.push(array[i - 1] + array[i - 2]);
+  }
 
-    return array;
+  return array;
 }
