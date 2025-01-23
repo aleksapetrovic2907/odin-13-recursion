22bda5b3898ceb1148314707205bd19de17970ce
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-23 01:07:17 +0100

    implement mergeSort() function

---
+++ sort.js
@@ -0,0 +1,12 @@
+function mergeSort(array) {
+    if (array.length <= 1) return array;
+
+    let mid = Math.floor(array.length / 2);
+    let leftHalf = array.slice(0, mid);
+    let rightHalf = array.slice(mid);
+
+    let sortedLeft = mergeSort(leftHalf);
+    let sortedRight = mergeSort(rightHalf);
+
+    return merge(sortedLeft, sortedRight);
+}
