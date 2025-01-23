f148fa91c02c95ebeff6812135d6301ec2789a7f
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-23 01:09:51 +0100

    format mergeSort() function with prettier

--- sort.js
+++ sort.js
@@ -1,12 +1,12 @@
 function mergeSort(array) {
-    if (array.length <= 1) return array;
+  if (array.length <= 1) return array;
 
-    let mid = Math.floor(array.length / 2);
-    let leftHalf = array.slice(0, mid);
-    let rightHalf = array.slice(mid);
+  let mid = Math.floor(array.length / 2);
+  let leftHalf = array.slice(0, mid);
+  let rightHalf = array.slice(mid);
 
-    let sortedLeft = mergeSort(leftHalf);
-    let sortedRight = mergeSort(rightHalf);
+  let sortedLeft = mergeSort(leftHalf);
+  let sortedRight = mergeSort(rightHalf);
 
-    return merge(sortedLeft, sortedRight);
+  return merge(sortedLeft, sortedRight);
 }
