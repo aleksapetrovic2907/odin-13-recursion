847e4d3e8974e64160aff1ead24e921af5269c78
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-24 01:11:40 +0100

    implement exponentialSearch()

--- search.js
+++ search.js
@@ -16,3 +16,23 @@ function binarySearch(sortedArray, value) {
 
   return -1;
 }
+
+function exponentialSearch(sortedArray, value) {
+  let lowerBoundIndex = 0;
+
+  for (let higherBoundIndex = 1; higherBoundIndex < sortedArray.length; ) {
+    if (value === sortedArray[higherBoundIndex]) {
+      return higherBoundIndex;
+    } else if (value < sortedArray[higherBoundIndex]) {
+      let boundedArray = sortedArray.slice(lowerBoundIndex, higherBoundIndex); // We add 1 since 2nd parameter is exclusive.
+      return lowerBoundIndex + binarySearch(boundedArray, value); // We add lowerBoundIndex to account for the sliced values left of bound.
+    }
+
+    lowerBoundIndex = higherBoundIndex;
+    higherBoundIndex = Math.min(higherBoundIndex * 2, sortedArray.length - 1);
+    console.table([lowerBoundIndex, higherBoundIndex]);
+  }
+
+  let boundedArray = sortedArray.slice(lowerBoundIndex, sortedArray.length);
+  return lowerBoundIndex + binarySearch(boundedArray, value);
+}
